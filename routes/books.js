9a3d89738c7469adf22dded822abdb93050febaa
Mobeen88 <mobeenali1238@gmail.com>
2025-11-13 18:12:53 +0000

    Final Changes

--- routes/books.js
+++ routes/books.js
@@ -6,9 +6,28 @@ router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
-router.get('/search-result', function (req, res, next) {
+router.get('/search_result', function (req, res, next) {
+    //Sets the keyword
+    let keyword = req.query.keyword;
+
+    //More accuracy
+    if(!keyword || keyword.trim() == "") {
+        return res.render("search_result.ejs", {booksFound: [], keyword: ""});
+    }
+
+    //Checks throught the database matching keywords
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    //
+    let searchValue = "%" + keyword + "%";
     //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    db.query(sqlquery, [searchValue], (err, result) =>{
+        if(err){
+            next(err);
+        }
+        else{
+            res.render("search-result.ejs", {booksFound: result, keyword: keyword} )
+        }
+    });
 });
 
 router.get('/list', function(req, res, next) {
@@ -18,7 +37,39 @@ router.get('/list', function(req, res, next) {
         if (err){
             next(err)
         }
-        res.send(result)
+        res.render("list.ejs", {availableBooks:result})
+    });
+});
+
+router.get('/addbook',function(req, res, next){
+    res.render('addbook.ejs')
+});
+
+router.post('/bookadded', function (req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        else
+            res.send(' This book is added to database, name: '+ req.body.name + ' price £'+ req.body.price)
+    })
+});
+
+router.get("/bargainbooks", function (req, res, next) {
+    //Query to get books with price less than 20
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+    //Execute SQL query
+    db.query(sqlquery, (err, result) => {
+        if (err){
+            next(err);
+        }
+        else{
+            res.render("bargainbooks.ejs", { bargainBooks: result });
+        }
     });
 });
 
